7f0d4bd04c25fa6199cc884f54ec69de7510e95f
abu-noman30 <nomannabil30@gmail.com>
2024-05-15 00:50:50 +0600

    first commit

---
+++ src/app/signaling-react-sdk/page.jsx
@@ -0,0 +1,11 @@
+import SignalingReactSDK from '@/components/Signaling/Signaling-sdk-react';
+
+const SignalingSDK = () => {
+  return (
+    <>
+      <SignalingReactSDK />
+    </>
+  );
+};
+
+export default SignalingSDK;
---
+++ src/app/signaling/page.tsx
@@ -0,0 +1,11 @@
+import SignalingManager from '../../components/Signaling/Signaling-sdk';
+
+const Signalling = () => {
+  return (
+    <>
+      <SignalingManager />
+    </>
+  );
+};
+
+export default Signalling;
